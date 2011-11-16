0cf95ae3462db5fadc4a2dc7942d141cd003dbda
Brett Wejrowski <brett@thewojogroup.com>
2011-11-16 01:25:42 -0500

    moved bind ready to private function

--- simpleCart.js
+++ simpleCart.js
@@ -443,46 +443,6 @@ generateSimpleCart = function(space){
 			
 		},
 		
-		// bind ready event used from jquery
-		bindReady: function() {
-
-			// Catch cases where $(document).ready() is called after the
-			// browser event has already occurred.
-			if ( document.readyState === "complete" ) {
-				// Handle it asynchronously to allow scripts the opportunity to delay ready
-				return setTimeout( simpleCart.init, 1 );
-			}
-
-			// Mozilla, Opera and webkit nightlies currently support this event
-			if ( document.addEventListener ) {
-				// Use the handy event callback
-				document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-
-				// A fallback to window.onload, that will always work
-				window.addEventListener( "load", simpleCart.init, false );
-
-			// If IE event model is used
-			} else if ( document.attachEvent ) {
-				// ensure firing before onload,
-				// maybe late but safe also for iframes
-				document.attachEvent( "onreadystatechange", DOMContentLoaded );
-
-				// A fallback to window.onload, that will always work
-				window.attachEvent( "onload", simpleCart.init );
-
-				// If IE and not a frame
-				// continually check to see if the document is ready
-				var toplevel = false;
-
-				try {
-					toplevel = window.frameElement == null;
-				} catch(e) {}
-
-				if ( document.documentElement.doScroll && toplevel ) {
-					doScrollCheck();
-				}
-			}
-		},
 		
 		error: function(message){
 			var msg = "";
@@ -495,6 +455,47 @@ generateSimpleCart = function(space){
 			simpleCart.trigger('error' , message );
 		}
 	});
+	
+ 	// bind ready event used from jquery
+	var sc_BindReady = function() {
+
+		// Catch cases where $(document).ready() is called after the
+		// browser event has already occurred.
+		if ( document.readyState === "complete" ) {
+			// Handle it asynchronously to allow scripts the opportunity to delay ready
+			return setTimeout( simpleCart.init, 1 );
+		}
+
+		// Mozilla, Opera and webkit nightlies currently support this event
+		if ( document.addEventListener ) {
+			// Use the handy event callback
+			document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+
+			// A fallback to window.onload, that will always work
+			window.addEventListener( "load", simpleCart.init, false );
+
+		// If IE event model is used
+		} else if ( document.attachEvent ) {
+			// ensure firing before onload,
+			// maybe late but safe also for iframes
+			document.attachEvent( "onreadystatechange", DOMContentLoaded );
+
+			// A fallback to window.onload, that will always work
+			window.attachEvent( "onload", simpleCart.init );
+
+			// If IE and not a frame
+			// continually check to see if the document is ready
+			var toplevel = false;
+
+			try {
+				toplevel = window.frameElement == null;
+			} catch(e) {}
+
+			if ( document.documentElement.doScroll && toplevel ) {
+				doScrollCheck();
+			}
+		}
+	};
 	
 	/*******************************************************************
 	 * 	TAX AND SHIPPING
@@ -1793,7 +1794,7 @@ generateSimpleCart = function(space){
 	}
 	
 	// bind the ready event
-	simpleCart.bindReady();	
+	sc_BindReady();	
 	
 	
 	return simpleCart;
