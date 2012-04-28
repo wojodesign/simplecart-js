3ed6e235d5bf925c2c6a5875b45e553f19c71278
Brett Wejrowski <brett@wejrowski.com>
2012-04-28 19:04:17 -0400

    bug in prototype selector creation

--- simpleCart.js
+++ simpleCart.js
@@ -1435,7 +1435,7 @@ Dual licensed under the MIT or GPL licenses.
 							return this.el[0].innerHTML;
 						}
 						this.each(function (e) {
-							simpleCart.$(e).update(text);
+							e.update(text);
 						});
 						return this;
 					},
@@ -1450,7 +1450,7 @@ Dual licensed under the MIT or GPL licenses.
 							return this.el[0].readAttribute(attr);
 						}
 						this.each(function (e) {
-							$engine(e).writeAttribute(attr, val);
+							e.writeAttribute(attr, val);
 						});
 						return this;
 					},
@@ -1458,29 +1458,29 @@ Dual licensed under the MIT or GPL licenses.
 						this.each(function (e) {
 							if (item.el) {
 								item.each(function (e2) {
-									$engine(e).appendChild(e2);
+									e.appendChild(e2);
 								});
 							} else if (isElement(item)) {
-								$engine(e).appendChild(item);
+								e.appendChild(item);
 							}
 						});
 						return this;
 					},
 					remove: function () {
 						this.each(function (e) {
-							$engine(e).remove();
+							e.remove();
 						});
 						return this;
 					},
 					addClass: function (klass) {
 						this.each(function (e) {
-							$engine(e).addClassName(klass);
+							e.addClassName(klass);
 						});
 						return this;
 					},
 					removeClass: function (klass) {
 						this.each(function (e) {
-							$engine(e).removeClassName(klass);
+							e.removeClassName(klass);
 						});
 						return this;
 					},
@@ -1493,13 +1493,13 @@ Dual licensed under the MIT or GPL licenses.
 					click: function (callback) {
 						if (isFunction(callback)) {
 							this.each(function (e) {
-								$engine(e).observe(_CLICK_, function (ev) {
+								e.observe(_CLICK_, function (ev) {
 									callback.call(e,ev);
 								});
 							});
 						} else if (isUndefined(callback)) {
 							this.each(function (e) {
-								$engine(e).fire(_CLICK_);
+								e.fire(_CLICK_);
 							});
 						}
 						return this;
@@ -1535,7 +1535,7 @@ Dual licensed under the MIT or GPL licenses.
 						if (isString(selector)) {
 							this.el = $engine(selector);
 						} else if (isElement(selector)) {
-							this.el = [$engine(selector)];
+							this.el = [selector];
 						}
 					}
 
