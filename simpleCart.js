9a990aac933b52c81cb83535b7142e50b41417e7
Hristo Chakarov <ickata@hristo-mb.bg.nc>
2013-07-24 17:42:48 +0300

    Fixed incompatibility with MooTools? 1.4.5 (no compatibility layer)

--- simpleCart.js
+++ simpleCart.js
@@ -1477,6 +1477,9 @@
 						return this;
 					},
 					create: function (selector) {
+						if ( ( window.typeOf || $type )( selector ) === 'array' ) {
+							selector = Array.flatten( selector );
+						}
 						this.el = $engine(selector);
 					}
 
