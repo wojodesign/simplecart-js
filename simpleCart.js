8bf6ef6274afab064f44ccdb2cda087a50a8c7a2
Sanam24 <mclarenf337@gmail.com>
2014-10-25 08:52:18 +0530

    Update simplecart.js
    Added an additional currency Indian Rupee

--- simpleCart.js
+++ simpleCart.js
@@ -63,6 +63,7 @@
 				// Currencies
 				currencies = {
 					"USD": { code: "USD", symbol: "&#36;", name: "US Dollar" },
+					"INR": { code: "INR", symbol: "&#x20B9", name: "Indian Rupee" },
 					"AUD": { code: "AUD", symbol: "&#36;", name: "Australian Dollar" },
 					"BRL": { code: "BRL", symbol: "R&#36;", name: "Brazilian Real" },
 					"CAD": { code: "CAD", symbol: "&#36;", name: "Canadian Dollar" },
